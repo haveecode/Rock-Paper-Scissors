325129e20c30a27446acc9c90858e3708de5d7d7
Haveecode <haveecode@gmail.com>
2022-08-02 01:16:57 -0400

    fixed game function: cycle breaks after 5 winning rounds

--- script.js
+++ script.js
@@ -15,25 +15,24 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
-        tiesScore++;
         return message = `It's a tie!`;
     } else if (playerSelection == `rock` && computerSelection == `paper`) {
-        computerScore++;
+        ++computerScore;
         return message = `You lose! Paper beats Rock`;
     } else if (playerSelection == `rock` && computerSelection == `scissors`) {
-        playerScore++;
+        ++playerScore;
         return message = `You won! Rock beats Scissors`;
     } else if (playerSelection == `paper` && computerSelection == `rock`) {
-        playerScore++;
+        ++playerScore;
         return message = `You won! Paper beats Rock`;
     } else if (playerSelection == `paper` && computerSelection == `scissors`) {
-        computerScore++;
+        ++computerScore;
         return message = `You lose! Scissors beats Paper`;
     } else if (playerSelection == `scissors` && computerSelection == `paper`) {
-        playerScore++;
+        ++playerScore;
         return message = `You won! Scissors beats Paper`;
     } else if (playerSelection == `scissors` && computerSelection == `rock`) {
-        computerScore++;
+        ++computerScore;
         return message = `You lose! Rock beats Scissors`;
     }
 
@@ -41,32 +40,29 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     
-    for (let i = 0; i < 5; i++) {
-        console.log(`Player: ${playerScore}, Computer: ${computerScore}, Ties: ${tiesScore}`);
+    for (let i = 1;; i++) {
+        console.log(`Player: ${playerScore}, Computer: ${computerScore}.`);
         const playerSelection = prompt(`Choose your weapon: Rock, Paper or Scissors?`).toLowerCase();
         const computerSelection = getComputerChoice();
-        console.log(`You selected ${playerSelection}`);
-        console.log(`Computer selected ${computerSelection}`);
+        console.log(`You selected ${playerSelection}, the computer selected ${computerSelection}.`);
         playRound(playerSelection, computerSelection);
         console.log(message);
         alert(message);
+
+        if (playerScore == 5 || computerScore == 5) break;
     }
 
-    if (i = 4 && playerScore > computerScore) {
-        console.log(`Player: ${playerScore} - Computer: ${computerScore} - Ties: ${tiesScore}. You are the winner!`);
+    if (playerScore == 5) {
+        console.log(`Player: ${playerScore} - Computer: ${computerScore}. You are the winner!`);
         alert(`With ${playerScore} win rounds, you beat the computer!`);
-    } else if (playerScore < computerScore) {
-        console.log(`Player: ${playerScore} - Computer: ${computerScore} - Ties: ${tiesScore}. You lose!`);
+    } else if (computerScore == 5) {
+        console.log(`Player: ${playerScore} - Computer: ${computerScore}. You lose!`);
         alert(`Oh no, the computer won ${computerScore} rounds, it's the winner!`);
-    } else if (playerScore == computerScore) {
-        console.log(`Player: ${playerScore} - Computer: ${computerScore} - Ties: ${tiesScore}. There was no winner! `);
-        alert(`There was no winner, it's a tie!`);
     }
     
 }
 
 let computerScore = 0;
 let playerScore = 0;
-let tiesScore = 0;
 let message = ``;
 game();
